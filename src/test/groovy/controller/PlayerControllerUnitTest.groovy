0d5be6037057f5819f4188d8dfbf53e37254db92
germano <germanosp11@hotmail.com>
2019-05-13 00:57:49 -0300

    Unit test under experimentation

--- src/test/groovy/controller/PlayerControllerUnitTest.groovy
+++ src/test/groovy/controller/PlayerControllerUnitTest.groovy
@@ -3,8 +3,11 @@ package controller
 
 import hive.player.controller.PlayerController
 import hive.player.entity.Player
+import hive.player.entity.PlayerOptions
+import hive.player.entity.PlayerSocial
 import hive.player.repository.PlayerRepository
 import org.mockito.Mock
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
@@ -24,20 +27,35 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 class PlayerControllerUnitTest extends Specification {
 
     def urlBase = 'http://localhost:9600/'
-    @Shared Optional<Player> player
+    @Shared Player player
     MockMvc mockMvc
     PlayerRepository playerRepository= Mock()
     def setup(){
-        when:
-        playerRepository.findById()
-        then:
-        player=playerRepository.findById()
+        player=new Player(
+                1,
+                "alias",
+                "email",
+                "11985054202",
+                "String grande",
+                "21/04/1998",
+                "46358570855",
+                new PlayerOptions(
+                        "ouro",
+                        "titan do café",
+                        "on",
+                        "on",
+                        "on",
+                        "on"),
+                new PlayerSocial("git","linkedin","@twitter"))
     }
     def "Should add an user"(){
         given:
         mockMvc = MockMvcBuilders.standaloneSetup(new PlayerController()).build()
+        and:
+        playerRepository.findById(1) >> player
         expect:
-        mockMvc.perform(get("$urlBase").header(AUTHENTICATED_USER_ID,1)).andDo(print())
+//        mockMvc?.perform(get("$urlBase/create")).andDo(print())
+        mockMvc?.perform(get("$urlBase").header(AUTHENTICATED_USER_ID,1)).andDo(print())
        // mockMvc?.perform(get("$urlBase")?.header(AUTHENTICATED_USER_ID,1))?.andExpect(status().isOk())
     }
 }
