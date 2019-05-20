83f7846b17224192cad69477fc100d896e15d4ec
germano <germanosp11@hotmail.com>
2019-05-20 00:04:35 -0300

    Removed typo

--- src/main/java/hive/player/repository/PlayerRepository.java
+++ src/main/java/hive/player/repository/PlayerRepository.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PlayerRepository extends JpaRepository<Player,Integer> {
 
-   @Query("from Player where autenticateUserId= ?1")
-   Player getByAutenticateUserId(String autenticateUserId);
+   @Query("from Player where authenticatedUserId= ?1")
+   Player findByAuthenticatedUserId(String authenticatedUserId);
 
-   @Query("select playerId from Player where autenticateUserId= ?1")
-   Integer getPlayerIdWithAutenticateUserId(String autenticateUserId);
+   @Query("select playerId from Player where authenticatedUserId= ?1")
+   Integer findPlayerIdWithAuthenticatedUserId(String authenticatedUserId);
 
 }
